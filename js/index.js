718b22e40d3eb1436b29d644c7dbae017946ce2a
Tzu-Yin Hong <lili668668@gmail.com>
2016-08-24 07:46:20 +0800

    update index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,26 @@
 $(function(){
-	var $grid = $('#imgs').masonry({
+    var baseurl = './midnight_photo/';
+    $.ajax({
+        url : 'img_paths.json',
+        dataType : 'json',
+        success : function( data ) {
+            for (var cnt = 0;cnt < data["paths"].length;cnt++) {
+                var img = '<div class="img"><img src="' + baseurl + data["paths"][cnt] + '"></div>';
+                $("#imgs").append(img);
+            }
+            var $grid = $('#imgs').masonry({
 				itemSelector: '.img',
 				animate: true
 			});
 			
-	$grid.imagesLoaded().progress( function() {
-		$grid.masonry();
-	});
-	
-	$grid.on('click', '.img', function(){
-		$(this).toggleClass('after-click');
-		$grid.masonry();
-	});
+            $grid.imagesLoaded().progress( function() {
+                $grid.masonry();
+            });
+            
+            $grid.on('click', '.img', function(){
+                $(this).toggleClass('after-click');
+                $grid.masonry();
+            });
+        }
+    });
 });
